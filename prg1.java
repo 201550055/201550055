5bf3e2397cd107a64495ff87f6910f93f7859cc7
201550055 <91643503+201550055@users.noreply.github.com>
2021-10-01 04:38:15 +0530

    Add files via upload

---
+++ prg1.java
@@ -0,0 +1,16 @@
+public class prg1 {
+    public void draw() {
+        System.out.println("Draw");
+    }
+
+    public void erase() {
+        System.out.println("Erase");
+    }
+
+    public static void main(String[] args)
+    {
+        prg1 obj=new prg1();
+        obj.draw();
+        obj.erase();
+    }
+}
